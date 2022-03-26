fdaf702d83a7cdbae19f281e763b923554db00d6
mohammed T <m.t.talib@hotmail.com>
2022-03-25 22:53:52 -0700

    Finished Structuredenglish

---
+++ test_1.py
@@ -0,0 +1,17 @@
+
+
+"""
+The program calculates the best payment option (better salary)
+first option is 100 dollarers , per day for 10 days
+second option is 1 a day with it being doubled ea day for 10 days, two functions to calculate pay rate,
+function1 calcs the amount for the first option
+function2 calcs the amount for the second option
+
+function1 will output 100*10 days
+function2 will loop 10 times, withe a time x2 (double amount) and add to total
+
+if amount is equal, we output to the user "option 1 and option 2 pays the same"
+if option1 is better, we output to the user "Option1 is better"
+if option2 is better, we output to the user "Option2 is better"
+
+"""
